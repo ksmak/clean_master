f66a7b8b5799334c932f41cef814721b37007e0c
Kairat Smakov <ksmakov@gmail.com>
2022-11-24 15:11:23 +0600

    UPDATE: create order section 2

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("index.html", menu=menu, contacts=contacts, whatsapp=whatsapp)
+    return render_template("index.html", 
+        menu=menu, 
+        contacts=contacts, 
+        whatsapp=whatsapp,
+        clean_list=clean_list
+    )
+
+@app.route("/send_order", methods=['POST'])
+def send_order():
+    return render_template("success.html")
 
 if __name__ == "__app__":
     app.run(host='localhost', port=5000, debug=True)
--- static/js/range.js
+++ static/js/range.js
@@ -1,14 +1,90 @@
-rangeElement = document.getElementById('room_size');
-rangeValueElement = document.getElementById('room_size_value');
-
-function updateRangeValue() {
-    rangeValueElement.textContent = rangeElement.value;
+function setVisible(range_box, val) {
+    if (val) {
+        range_box.style.display = 'block';
+    } else {
+        range_box.style.display = 'none';
+    }
 }
 
-rangeElement.addEventListener("input", function(e) {
-    updateRangeValue();
-}, false);
+function updateRangeValue(element, val) {
+    element.textContent = val;
+}
 
-rangeElement.addEventListener("change", function(e) {
-    updateRangeValue();
-}, false);
+function updateCost() {
+    const form = document.forms[0];
+    let cost = 0;
+    for (el of clean_list) {
+        if (form.clean_type.value == el.name) {
+            cost += el.cost * form[el.name + '_range'].value;
+        }
+        if (el.group == 'cleanning') {
+            if (form[el.name].checked) {
+                if (el.is_range) {
+                    cost += el.cost * form[el.name + '_range'].value;
+                } else {
+                    cost += el.cost;
+                }
+            }
+        }
+    }
+    const costEl = document.getElementById("cost");
+    costEl.textContent = cost + " тг.";
+}
+
+for (element of clean_list) {
+
+    if (element.is_range) {
+        const radio = document.getElementById(element.name);
+        const rangeBox = document.getElementById(element.name + "_range_box");
+        const range = document.getElementById(element.name + "_range");
+        
+        setVisible(rangeBox, false);
+    
+        radio.addEventListener("change", function(e) {
+            for (el of clean_list) {
+                if (el.is_range) {
+                    const radio = document.getElementById(el.name);
+                    const rangeBox = document.getElementById(el.name + "_range_box");
+                    
+                    setVisible(rangeBox, radio.checked);
+                }
+            }
+
+        }, false);
+        
+        range.addEventListener("input", function(e) {
+            for (el of clean_list) {
+                if (el.is_range) {
+                    const range = document.getElementById(el.name + "_range");
+                    const current_value = document.getElementById(el.name + "_value");
+                    
+                    if (range == e.target) {
+                        updateRangeValue(current_value, e.target.value);
+                        updateCost();
+                        break;
+                    }
+                }
+            }
+        }, false);
+        
+        range.addEventListener("change", function(e) {
+            for (el of clean_list) {
+                if (el.is_range) {
+                    const range = document.getElementById(element.name + "_range");
+                    const current_value = document.getElementById(element.name + "_value");
+                    
+                    if (range == e.target) {
+                        updateRangeValue(current_value, e.target.value);
+                        break;
+                    }
+                }
+            }
+        }, false); 
+    }
+
+    const radio = document.getElementById(element.name);
+
+    radio.addEventListener("click", function(e) {
+        updateCost();
+    }, false);
+}
